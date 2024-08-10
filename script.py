922fe9d62e751c66f9803d9189d4db68c5897b99
KirGorbunov <106156470+KirGorbunov@users.noreply.github.com>
2024-08-10 15:02:59 +0300

    change structure, add functions

--- script.py
+++ script.py
@@ -1,7 +1,28 @@
 import json
 import pandas as pd
+import logging
+
 
 from settings import settings
+
+
+logging.basicConfig(
+    level='DEBUG',
+    format="{levelname} - {message}",
+    style="{",
+)
+
+
+def preparing_data(excel_signals: pd.DataFrame, excel_devices: pd.DataFrame) -> pd.DataFrame:
+    excel_devices = excel_devices.rename(
+        columns={settings.DEVICES_SHEET_DEVICE_COLUMN_NAME: settings.SIGNALS_SHEET_DEVICE_COLUMN_NAME}
+    )
+    general = pd.merge(excel_signals,
+                       excel_devices,
+                       on=settings.SIGNALS_SHEET_DEVICE_COLUMN_NAME,
+                       how='left')
+    signals = general.loc[general[settings.SIGNALS_SHEET_SIGNAL_TYPE_COLUMN_NAME] == settings.ONLY_SIGNALS_TYPE]
+    return signals
 
 
 def get_uniq_addresses(signals: pd.DataFrame) -> dict:
@@ -31,53 +52,72 @@ def get_not_uniq_addresses(signals: pd.DataFrame, gateway: str) -> dict:
     return complex_signals
 
 
-def excel_to_json(excel_signals: pd.DataFrame, excel_devices: pd.DataFrame):
-    excel_devices = excel_devices.rename(
-        columns={settings.DEVICES_SHEET_DEVICE_COLUMN_NAME: settings.SIGNALS_SHEET_DEVICE_COLUMN_NAME}
-    )
-    general = pd.merge(excel_signals,
-                       excel_devices,
-                       on=settings.SIGNALS_SHEET_DEVICE_COLUMN_NAME,
-                       how='left')
-    signals = general.loc[general[settings.SIGNALS_SHEET_SIGNAL_TYPE_COLUMN_NAME] == settings.ONLY_SIGNALS_TYPE]
-    uniqe_gateway = signals[settings.DEVICES_SHEET_GATEWAY_COLUMN_NAME].unique()
-    slaves = {}
-    signals_for_excel = {}
+def get_signals_for_excel(union_addresses: dict, signals_for_excel: dict) -> dict:
+    for code in union_addresses.values():
+        signals_for_excel[code] = {
+            "type": "ushort-float32",
+            "base": [settings.EXCEL_DATA_FILE_NAME, code]
+        }
+    return signals_for_excel
 
-    for gateway in uniqe_gateway:
-        device_signals = signals.loc[signals[settings.DEVICES_SHEET_GATEWAY_COLUMN_NAME] == gateway]
-        uniq_addresses = get_uniq_addresses(device_signals)
-        not_uniq_addresses = get_not_uniq_addresses(device_signals, gateway)
-        union_addresses = uniq_addresses | not_uniq_addresses
-        for code in union_addresses.values():
-            signals_for_excel[code] = {
-                "type": "ushort-float32",
-                "base": [settings.EXCEL_DATA_FILE_NAME, code]
-            }
+def get_slaves(gateway: str, device_signals: dict, union_addresses: dict, slaves: dict) -> dict:
+    if union_addresses:
+        slaves[gateway] = {
+            "slaveID": int(device_signals.iloc[0][settings.DEVICES_SHEET_COMMON_ADDRESS_COLUMN_NAME]),
+            "holdings": union_addresses
+        }
+    return slaves
 
-        if union_addresses:
-            slaves[gateway] = {
-                "slaveID": int(device_signals.iloc[0][settings.DEVICES_SHEET_COMMON_ADDRESS_COLUMN_NAME]),
-                "holdings": union_addresses
-            }
-
+def creating_files(signals_for_excel: dict, config: dict):
     all_codes = pd.DataFrame(columns=signals_for_excel.keys())
     all_codes.to_excel(settings.EXCEL_DATA_FILE_NAME)
 
-    config = {
-        "signals": signals_for_excel,
-        "servers": {
-            "Test": {
-                "host": settings.HOST,
-                "port": settings.PORT,
-                "period": [settings.HOURS, settings.MINUTS, settings.SECONDS],
-                "slaves": slaves
-            }
-        }
-    }
-
     with open(settings.JSON_CONFIG_FILE_NAME, 'w', encoding='utf-8') as json_file:
         json.dump(config, json_file, ensure_ascii=False)
+
+
+def excel_to_json(excel_signals: pd.DataFrame, excel_devices: pd.DataFrame):
+    try:
+        signals = preparing_data(excel_signals, excel_devices)
+        logging.debug('Data prepared')
+    except:
+        logging.exception("Can't prepared data")
+
+
+    try:
+        uniqe_gateway = signals[settings.DEVICES_SHEET_GATEWAY_COLUMN_NAME].unique()
+        signals_for_excel = {}
+        slaves = {}
+
+        for gateway in uniqe_gateway:
+            device_signals = signals.loc[signals[settings.DEVICES_SHEET_GATEWAY_COLUMN_NAME] == gateway]
+            uniq_addresses = get_uniq_addresses(device_signals)
+            not_uniq_addresses = get_not_uniq_addresses(device_signals, gateway)
+            union_addresses = uniq_addresses | not_uniq_addresses
+            signals_for_excel = get_signals_for_excel(union_addresses, signals_for_excel)
+            slaves = get_slaves(gateway, device_signals, union_addresses, slaves)
+
+        config = {
+            "signals": signals_for_excel,
+            "servers": {
+                "Test": {
+                    "host": settings.HOST,
+                    "port": settings.PORT,
+                    "period": [settings.HOURS, settings.MINUTS, settings.SECONDS],
+                    "slaves": slaves
+                }
+            }
+        }
+        logging.debug('Config have been created successfully')
+    except:
+        logging.exception("Oops! Can't creating signals_for_excel and slaves dict")
+
+    try:
+        creating_files(signals_for_excel, config)
+        logging.info(f"Files {settings.EXCEL_DATA_FILE_NAME} and {settings.JSON_CONFIG_FILE_NAME} "
+                     f"have been created successfully")
+    except:
+        logging.exception('Error creating files')
 
 
 if __name__ == "__main__":
@@ -94,4 +134,6 @@ if __name__ == "__main__":
                                            settings.DEVICES_SHEET_DEVICE_COLUMN_NAME,
                                            settings.DEVICES_SHEET_COMMON_ADDRESS_COLUMN_NAME],
                                   dtype='string')
+    logging.info(f"Script starting with sheets signals (len={excel_signals.shape[0]}) "
+                 f"and devices (len={excel_devices.shape[0]})")
     excel_to_json(excel_signals, excel_devices)
