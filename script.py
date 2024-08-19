60927bc01e819d548af7a1201c4131caad146792
Kirill Gorbunov <kirgorbunov@gmail.com>
2024-08-19 14:06:18 +0300

    Change constants naming

--- script.py
+++ script.py
@@ -14,22 +14,22 @@ logging.basicConfig(
 
 def preparing_data(excel_signals: pd.DataFrame, excel_devices: pd.DataFrame) -> pd.DataFrame:
     excel_devices = excel_devices.rename(
-        columns={settings.DEVICES_SHEET_DEVICE_COLUMN_NAME: settings.SIGNALS_SHEET_DEVICE_COLUMN_NAME}
-    )
+        columns={settings.DEVICES_SHEET_DEVICE_COLUMN: settings.SIGNALS_SHEET_DEVICE_COLUMN}
+)
     general = pd.merge(excel_signals,
                        excel_devices,
-                       on=settings.SIGNALS_SHEET_DEVICE_COLUMN_NAME,
+                       on=settings.SIGNALS_SHEET_DEVICE_COLUMN,
                        how="left")
-    signals = general.loc[general[settings.SIGNALS_SHEET_SIGNAL_TYPE_COLUMN_NAME] == settings.ONLY_SIGNALS_TYPE]
+    signals = general.loc[general[settings.SIGNALS_SHEET_SIGNAL_TYPE_COLUMN] == settings.ONLY_SIGNALS_TYPE]
     return signals
 
 def dataframe_to_dicts(signals: pd.DataFrame) -> (dict):
-    uniqe_gateway = signals[settings.DEVICES_SHEET_GATEWAY_COLUMN_NAME].unique()
+    uniqe_gateway = signals[settings.DEVICES_SHEET_GATEWAY_COLUMN].unique()
     signals_for_excel = {}
     slaves = {}
 
     for gateway in uniqe_gateway:
-        device_signals = signals.loc[signals[settings.DEVICES_SHEET_GATEWAY_COLUMN_NAME] == gateway]
+        device_signals = signals.loc[signals[settings.DEVICES_SHEET_GATEWAY_COLUMN] == gateway]
         uniq_addresses = get_uniq_addresses(device_signals)
         not_uniq_addresses = get_not_uniq_addresses(device_signals, gateway)
         union_addresses = uniq_addresses | not_uniq_addresses
@@ -51,25 +51,25 @@ def dataframe_to_dicts(signals: pd.DataFrame) -> (dict):
     return signals_for_excel, config
 
 def get_uniq_addresses(signals: pd.DataFrame) -> dict:
-    uniq_signals = signals.drop_duplicates(subset=[settings.SIGNALS_SHEET_ADDRESS_COLUMN_NAME], keep=False)
-    uniq_signals.loc[:, settings.SIGNALS_SHEET_CODE_COLUMN_NAME] = (
-            uniq_signals[settings.SIGNALS_SHEET_CODE_COLUMN_NAME]
+    uniq_signals = signals.drop_duplicates(subset=[settings.SIGNALS_SHEET_ADDRESS_COLUMN], keep=False)
+    uniq_signals.loc[:, settings.SIGNALS_SHEET_CODE_COLUMN] = (
+            uniq_signals[settings.SIGNALS_SHEET_CODE_COLUMN]
             + "_"
-            + uniq_signals[settings.DEVICES_SHEET_GATEWAY_COLUMN_NAME]
+            + uniq_signals[settings.DEVICES_SHEET_GATEWAY_COLUMN]
     )
     uniq_address = uniq_signals.set_index(
-        settings.SIGNALS_SHEET_ADDRESS_COLUMN_NAME
+        settings.SIGNALS_SHEET_ADDRESS_COLUMN
     )[
-        settings.SIGNALS_SHEET_CODE_COLUMN_NAME
+        settings.SIGNALS_SHEET_CODE_COLUMN
     ].to_dict()
     return uniq_address
 
 def get_not_uniq_addresses(signals: pd.DataFrame, gateway: str) -> dict:
     complex_signals = {}
-    signals_addr_not_uniq = signals[signals.duplicated(subset=[settings.SIGNALS_SHEET_ADDRESS_COLUMN_NAME], keep=False)]
-    for address in signals_addr_not_uniq[settings.SIGNALS_SHEET_ADDRESS_COLUMN_NAME].unique():
+    signals_addr_not_uniq = signals[signals.duplicated(subset=[settings.SIGNALS_SHEET_ADDRESS_COLUMN], keep=False)]
+    for address in signals_addr_not_uniq[settings.SIGNALS_SHEET_ADDRESS_COLUMN].unique():
         signals_with_same_address = signals_addr_not_uniq.loc[
-            signals_addr_not_uniq[settings.SIGNALS_SHEET_ADDRESS_COLUMN_NAME] == address
+            signals_addr_not_uniq[settings.SIGNALS_SHEET_ADDRESS_COLUMN] == address
         ]
         if not signals_with_same_address.empty:
             complex_signals[address] = f"{len(signals_with_same_address)}_signals_{gateway}_{address}"
@@ -79,23 +79,23 @@ def get_signals_for_excel(union_addresses: dict, signals_for_excel: dict) -> dic
     for code in union_addresses.values():
         signals_for_excel[code] = {
             "type": "ushort-float32",
-            "base": [settings.EXCEL_DATA_FILE_NAME, code]
+            "base": [settings.EXCEL_DATA_FILE, code]
         }
     return signals_for_excel
 
 def get_slaves(gateway: str, device_signals: dict, union_addresses: dict, slaves: dict) -> dict:
     if union_addresses:
         slaves[gateway] = {
-            "slaveID": int(device_signals.iloc[0][settings.DEVICES_SHEET_COMMON_ADDRESS_COLUMN_NAME]),
+            "slaveID": int(device_signals.iloc[0][settings.DEVICES_SHEET_COMMON_ADDRESS_COLUMN]),
             "holdings": union_addresses
         }
     return slaves
 
 def creating_files(signals_for_excel: dict, config: dict):
     all_codes = pd.DataFrame(columns=signals_for_excel.keys())
-    all_codes.to_excel(settings.EXCEL_DATA_FILE_NAME)
+    all_codes.to_excel(settings.EXCEL_DATA_FILE)
 
-    with open(settings.JSON_CONFIG_FILE_NAME, "w", encoding="utf-8") as json_file:
+    with open(settings.JSON_CONFIG_FILE, "w", encoding="utf-8") as json_file:
         json.dump(config, json_file, ensure_ascii=False)
 
 def excel_to_json(excel_signals: pd.DataFrame, excel_devices: pd.DataFrame):
@@ -106,23 +106,23 @@ def excel_to_json(excel_signals: pd.DataFrame, excel_devices: pd.DataFrame):
     logging.debug("Config have been created successfully")
 
     creating_files(signals_for_excel, config)
-    logging.info(f"Files {settings.EXCEL_DATA_FILE_NAME} and {settings.JSON_CONFIG_FILE_NAME} "
+    logging.info(f"Files {settings.EXCEL_DATA_FILE} and {settings.JSON_CONFIG_FILE} "
                  f"have been created successfully")
 
 
 if __name__ == "__main__":
-    excel_signals = pd.read_excel(settings.LIST_OF_SIGNALS_FILE_NAME,
-                                  sheet_name=settings.SIGNALS_SHEET_NAME,
-                                  usecols=[settings.SIGNALS_SHEET_DEVICE_COLUMN_NAME,
-                                           settings.SIGNALS_SHEET_CODE_COLUMN_NAME,
-                                           settings.SIGNALS_SHEET_SIGNAL_TYPE_COLUMN_NAME,
-                                           settings.SIGNALS_SHEET_ADDRESS_COLUMN_NAME],
+    excel_signals = pd.read_excel(settings.LIST_OF_SIGNALS_FILE,
+                                  sheet_name=settings.SIGNALS_SHEET,
+                                  usecols=[settings.SIGNALS_SHEET_DEVICE_COLUMN,
+                                           settings.SIGNALS_SHEET_CODE_COLUMN,
+                                           settings.SIGNALS_SHEET_SIGNAL_TYPE_COLUMN,
+                                           settings.SIGNALS_SHEET_ADDRESS_COLUMN],
                                   dtype="string")
-    excel_devices = pd.read_excel(settings.LIST_OF_SIGNALS_FILE_NAME,
-                                  settings.DEVICES_SHEET_NAME,
-                                  usecols=[settings.DEVICES_SHEET_GATEWAY_COLUMN_NAME,
-                                           settings.DEVICES_SHEET_DEVICE_COLUMN_NAME,
-                                           settings.DEVICES_SHEET_COMMON_ADDRESS_COLUMN_NAME],
+    excel_devices = pd.read_excel(settings.LIST_OF_SIGNALS_FILE,
+                                  settings.DEVICES_SHEET,
+                                  usecols=[settings.DEVICES_SHEET_GATEWAY_COLUMN,
+                                           settings.DEVICES_SHEET_DEVICE_COLUMN,
+                                           settings.DEVICES_SHEET_COMMON_ADDRESS_COLUMN],
                                   dtype="string")
     logging.info(f"Script starting with sheets signals (len={excel_signals.shape[0]}) "
                  f"and devices (len={excel_devices.shape[0]})")
--- settings.py
+++ settings.py
@@ -2,25 +2,33 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    LIST_OF_SIGNALS_FILE_NAME: str
+    # Названия файлов:
+    LIST_OF_SIGNALS_FILE: str
+    EXCEL_DATA_FILE: str = "data.xlsx"
+    JSON_CONFIG_FILE: str = "config.json"
 
-    SIGNALS_SHEET_NAME: str = "signals"
-    SIGNALS_SHEET_DEVICE_COLUMN_NAME: str = "device"
-    SIGNALS_SHEET_CODE_COLUMN_NAME: str = "code"
-    SIGNALS_SHEET_SIGNAL_TYPE_COLUMN_NAME: str = "signal_type"
-    SIGNALS_SHEET_ADDRESS_COLUMN_NAME: str = "address"
+    # Названия страниц в excel файле:
+    SIGNALS_SHEET: str = "signals"
+    DEVICES_SHEET: str = "devices"
+
+    # Названия столбцов:
+    SIGNALS_SHEET_DEVICE_COLUMN: str = "device"
+    SIGNALS_SHEET_CODE_COLUMN: str = "code"
+    SIGNALS_SHEET_SIGNAL_TYPE_COLUMN: str = "signal_type"
+    SIGNALS_SHEET_ADDRESS_COLUMN: str = "address"
+
+    DEVICES_SHEET_GATEWAY_COLUMN: str = "gateway"
+    DEVICES_SHEET_DEVICE_COLUMN: str = "code"
+    DEVICES_SHEET_COMMON_ADDRESS_COLUMN: str = "common_address"
+
+    # Необходимый тип сигнала:
     ONLY_SIGNALS_TYPE: str = "Сигнал"
 
-    DEVICES_SHEET_NAME: str = "devices"
-    DEVICES_SHEET_GATEWAY_COLUMN_NAME: str = "gateway"
-    DEVICES_SHEET_DEVICE_COLUMN_NAME: str = "code"
-    DEVICES_SHEET_COMMON_ADDRESS_COLUMN_NAME: str = "common_address"
-
-    EXCEL_DATA_FILE_NAME: str = "data.xlsx"
-
-    JSON_CONFIG_FILE_NAME: str = "config.json"
+    # IP-адрес и порт:
     HOST: str = "127.0.0.1"
     PORT: int = 502
+
+    # Конфигурирование таймера эмулятора:
     HOURS: int = 0
     MINUTS: int = 10
     SECONDS: int = 0
@@ -29,45 +37,3 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
-
-
-
-
-
-
-
-
-
-
-
-#     POSTGRES_DB: str
-#     POSTGRES_USER: str
-#     POSTGRES_PASSWORD: str
-#     POSTGRES_HOST: str = "127.0.0.1"
-#     POSTGRES_PORT: str = "5432"
-#     ELASTIC_PORT: str = "9200"
-#     ELASTIC_HOST: str = "localhost"
-#
-#     PERSON_LIMIT: str = "50"
-#     GENRE_LIMIT: str = "1"
-#     FILMWORK_LIMIT: str = "200"
-#     RELAX_TIME: int = 2
-#
-#     TIMER_GENRES: str = "2020-06-16T20:14:09.310000+00:00"
-#     TIMER_PERSONS: str = "2020-06-16T20:14:09.310000+00:00"
-#     TIMER_FILMWORKS: str = "2020-06-16T20:14:09.310000+00:00"
-#
-#     model_config = SettingsConfigDict(env_file="../.env", env_file_encoding="utf-8")
-#
-#     @property
-#     def dsl(self) -> dict:
-#         return {
-#             "dbname": self.POSTGRES_DB,
-#             "user": self.POSTGRES_USER,
-#             "password": self.POSTGRES_PASSWORD,
-#             "host": self.POSTGRES_HOST,
-#             "port": self.POSTGRES_PORT,
-#             "options": "-c search_path=content"
-#         }
-#
-#
